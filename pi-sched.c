1bfdc04c692483a27656ed0dcb93557d0987935f
Anne Gatchell <annegatchell@gmail.com>
2013-03-22 17:15:55 -0600

    Added handling for another argument in pi-sched.c

--- pi-sched.c
+++ pi-sched.c
@@ -19,6 +19,7 @@
 
 #define DEFAULT_ITERATIONS 1000000
 #define RADIUS (RAND_MAX / 2)
+#define DEFAULT_NUM_PROCESSES 1
 
 inline double dist(double x0, double y0, double x1, double y1){
     return sqrt(pow((x1-x0),2) + pow((y1-y0),2));
@@ -33,6 +34,7 @@ int main(int argc, char* argv[]){
     long i;
     long iterations;
     struct sched_param param;
+    int num_processes;
     int policy;
     double x, y;
     double inCircle = 0.0;
@@ -43,35 +45,51 @@ int main(int argc, char* argv[]){
     /* Process program arguments to select iterations and policy */
     /* Set default iterations if not supplied */
     if(argc < 2){
-	iterations = DEFAULT_ITERATIONS;
+        iterations = DEFAULT_ITERATIONS;
     }
     /* Set default policy if not supplied */
     if(argc < 3){
-	policy = SCHED_OTHER;
+	   policy = SCHED_OTHER;
+    }
+    /*Set default number of processes */
+    if(argc < 4){
+        num_processes = DEFAULT_NUM_PROCESSES;
     }
     /* Set iterations if supplied */
     if(argc > 1){
-	iterations = atol(argv[1]);
-	if(iterations < 1){
-	    fprintf(stderr, "Bad iterations value\n");
-	    exit(EXIT_FAILURE);
-	}
+    	iterations = atol(argv[1]);
+    	if(iterations < 1){
+    	    fprintf(stderr, "Bad iterations value\n");
+    	    exit(EXIT_FAILURE);
+    	}
     }
     /* Set policy if supplied */
     if(argc > 2){
-	if(!strcmp(argv[2], "SCHED_OTHER")){
-	    policy = SCHED_OTHER;
-	}
-	else if(!strcmp(argv[2], "SCHED_FIFO")){
-	    policy = SCHED_FIFO;
-	}
-	else if(!strcmp(argv[2], "SCHED_RR")){
-	    policy = SCHED_RR;
-	}
-	else{
-	    fprintf(stderr, "Unhandeled scheduling policy\n");
-	    exit(EXIT_FAILURE);
-	}
+    	if(!strcmp(argv[2], "SCHED_OTHER")){
+    	    policy = SCHED_OTHER;
+    	}
+    	else if(!strcmp(argv[2], "SCHED_FIFO")){
+    	    policy = SCHED_FIFO;
+    	}
+    	else if(!strcmp(argv[2], "SCHED_RR")){
+    	    policy = SCHED_RR;
+    	}
+    	else{
+    	    fprintf(stderr, "Unhandeled scheduling policy\n");
+    	    exit(EXIT_FAILURE);
+    	}
+    }
+
+    if(argc > 3){
+        num_processes = atoi(argv[3]);
+        if(num_processes < 1){
+            fprintf(stderr, "Bad number of processes value\n");
+            exit(EXIT_FAILURE);
+        }
+        else if(num_processes > 1000){
+            fprintf(stderr, "Bad number of processes value\n");
+            exit(EXIT_FAILURE);
+        }
     }
     
     /* Set process to max prioty for given scheduler */
@@ -81,19 +99,19 @@ int main(int argc, char* argv[]){
     fprintf(stdout, "Current Scheduling Policy: %d\n", sched_getscheduler(0));
     fprintf(stdout, "Setting Scheduling Policy to: %d\n", policy);
     if(sched_setscheduler(0, policy, &param)){
-	perror("Error setting scheduler policy");
-	exit(EXIT_FAILURE);
+    	perror("Error setting scheduler policy");
+    	exit(EXIT_FAILURE);
     }
     fprintf(stdout, "New Scheduling Policy: %d\n", sched_getscheduler(0));
 
     /* Calculate pi using statistical methode across all iterations*/
     for(i=0; i<iterations; i++){
-	x = (random() % (RADIUS * 2)) - RADIUS;
-	y = (random() % (RADIUS * 2)) - RADIUS;
-	if(zeroDist(x,y) < RADIUS){
-	    inCircle++;
-	}
-	inSquare++;
+    	x = (random() % (RADIUS * 2)) - RADIUS;
+    	y = (random() % (RADIUS * 2)) - RADIUS;
+    	if(zeroDist(x,y) < RADIUS){
+    	    inCircle++;
+    	}
+    	inSquare++;
     }
 
     /* Finish calculation */
